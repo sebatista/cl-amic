185ce30b3053038936e271d77826177f84b87b5d
jobiols <jorge.obiols@gmail.com>
2019-08-09 20:38:54 -0300

    [ADD] Version 12

--- amic_default/__manifest__.py
+++ amic_default/__manifest__.py
@@ -19,7 +19,7 @@
 # -----------------------------------------------------------------------------
 {
     'name': 'AMIC',
-    'version': '11.0.0.0.0',
+    'version': '12.0.0.0.0',
     'license': 'Other OSI approved licence',
     'category': 'Default Application',
     'summary': 'Customization for AMIC',
@@ -31,10 +31,8 @@
         'purchase',
 
         # minimum modules for argentinian localizacion + utilities + fixes
-        'standard_depends_ce',
+        # 'standard_depends_ce',
 
-        # utilitarios adicionales
-        'backend_theme',
     ],
     'data': [
     ],
@@ -49,47 +47,23 @@
     # Here begins docker-odoo-environment manifest
     # --------------------------------------------
 
-    # if Enterprise it installs in a different directory than community
-    'Enterprise': False,
-
     # port where odoo starts serving pages
     'port': '8069',
-
-    # syntax version of repos and images
-    'env_ver': 1,
-
-    # example repos version 1
     'repos': [
-        {'usr': 'jobiols', 'repo': 'cl-amic', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'odoo-addons', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'rafi16jan-backend-theme',
-         'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'cl-amic', 'branch': '12.0'},
+        {'usr': 'jobiols', 'repo': 'odoo-addons', 'branch': '12.0'},
 
-        {'usr': 'jobiols', 'repo': 'adhoc-odoo-argentina', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'adhoc-argentina-sale', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'adhoc-account-financial-tools',
-         'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'adhoc-account-payment', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'adhoc-miscellaneous', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'adhoc-argentina-reporting',
-         'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'adhoc-reporting-engine', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'adhoc-aeroo_reports', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'adhoc-sale', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'adhoc-odoo-support', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'adhoc-product', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'oca-partner-contact', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'oca-partner-contact', 'branch': '12.0'},
         {'usr': 'jobiols', 'repo': 'oca-web', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'oca-server-tools', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'oca-server-tools', 'branch': '12.0'},
         {'usr': 'jobiols', 'repo': 'oca-social', 'branch': '11.0'},
-        {'usr': 'jobiols', 'repo': 'oca-server-brand', 'branch': '11.0'},
+        {'usr': 'jobiols', 'repo': 'oca-server-brand', 'branch': '12.0'},
     ],
 
     'docker': [
-        {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-jeo', 'ver': '11.0'},
-        {'name': 'postgres', 'usr': 'postgres', 'ver': '11.1-alpine'},
+        {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-jeo', 'ver': '12.0'},
+        {'name': 'postgres', 'usr': 'postgres', 'ver': '10.1-alpine'},
         {'name': 'nginx', 'usr': 'nginx', 'ver': 'latest'},
         {'name': 'aeroo', 'usr': 'adhoc', 'img': 'aeroo-docs'},
     ],
-
 }
