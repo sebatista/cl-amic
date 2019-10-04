68a4d33f40451d47e5bf62f3a211d2a217bcf767
jobiols <jorge.obiols@gmail.com>
2019-10-04 17:02:29 -0300

    [WIP] agregando adjuntos

--- mrp_ot/models/mrp_bom.py
+++ mrp_ot/models/mrp_bom.py
@@ -17,5 +17,28 @@ class ModelName(models.Model):
     def print_ot(self):
         """ Imprimir la OT
         """
+        raise Warning('Funcionalidad no implementada')
 
-        raise Warning('Todavia no esta implementada la impresion')
+    def action_see_attachments(self):
+        """ Mostrar y dejar agregar los attach
+        """
+        domain = [('res_model', '=', 'mrp.bom'),
+                  ('res_id', '=', self.id)]
+        attachment_view = self.env.ref('mrp_ot.view_document_file_kanban_mrp_ot')
+        return {
+            'name': 'Adjuntos',
+            'domain': domain,
+            'res_model': 'mrp.bom',
+            'type': 'ir.actions.act_window',
+            'view_id': attachment_view.id,
+            'views': [(attachment_view.id, 'kanban'), (False, 'form')],
+            'view_mode': 'kanban,tree,form',
+            'view_type': 'form',
+            'help': '''<p class="oe_view_nocontent_create">
+                        Haga click aqui para subir los documentos de la OT.
+                    </p><p>
+                        Use esta caracteristica para almacenar documentos tipo PDF.
+                    </p>''',
+            'limit': 80,
+            'context': "{'default_res_model': '%s','default_res_id': %d}" % ('mrp.bom', self.id)
+        }
---
+++ mrp_ot/models/mrp_bom_document.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class MrpBomDocument(models.Model):
+    """ Extension of ir.attachment only used in MRP BOM to handle archivage
+        of OT documents.
+    """
+    _name = 'mrp.bom.document'
+    _description = "OT Document"
+    _inherits = {
+        'ir.attachment': 'ir_attachment_id',
+    }
+    _order = "priority desc, id desc"
+
+    ir_attachment_id = fields.Many2one(
+        'ir.attachment',
+        string='Adjuntos relacionados',
+        required=True,
+        ondelete='cascade'
+    )
+    active = fields.Boolean(
+        'Active',
+        default=True
+    )
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Bajo'),
+        ('2', 'Alto'),
+        ('3', 'Muy alto')],
+        string="Prioridad",
+        help='Define el orden cuando se muestran los documentos de la OT.'
+    )
