2d6ecf3478fef1e8bf367e34d8ae7359717e23ef
jobiols <jorge.obiols@gmail.com>
2019-10-04 11:10:20 -0300

    [REF] rename lot_attributes -> mrp_lot_attributes

--- mrp_lot_attributes/__manifest__.py
+++ mrp_lot_attributes/__manifest__.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Lot Attributes",
+    "name": "MRP Lot Attributes",
     "summary": "Add attributes to lot",
     "version": "11.0.0.0.0",
     "development_status": "Alpha",
