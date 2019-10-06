41b4a8033d9e224dc05a3ae72b34dcd318261e77
jobiols <jorge.obiols@gmail.com>
2019-10-06 12:15:28 -0300

    [WIP] travis

--- amic_default/__manifest__.py
+++ amic_default/__manifest__.py
@@ -83,6 +83,7 @@
         {'usr': 'oca', 'repo': 'manufacture', 'branch': '11.0'},
         {'usr': 'oca', 'repo': 'management-system', 'branch': '11.0'},
         {'usr': 'oca', 'repo': 'sale-workflow', 'branch': '11.0'},
+        {'usr': 'oca', 'repo': 'stock-logistics-warehouse', 'branch': '11.0'},
 
     ],
 
