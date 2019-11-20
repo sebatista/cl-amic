9c62113d1051e098e738bfaf77701b1fd9bf371b
jobiols <jorge.obiols@gmail.com>
2019-11-20 00:46:24 -0300

    [FIX] travis

--- mrp_lot_attributes/models/production_lot.py
+++ mrp_lot_attributes/models/production_lot.py
@@ -6,7 +6,7 @@ from openerp import models, fields
 class ProductionLot(models.Model):
     _inherit = "stock.production.lot"
 
-    #name = fields.Char(
+    # name = fields.Char(
     #    'Lote/Numero de Ingreso',
     #    default=lambda self: '%s' % self.env['ir.sequence'].next_by_code(
     #        'stock.lot.serial'),
