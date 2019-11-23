3a2eff2f4dfd0c9f3e867a407d91dd94dee52f14
jobiols <jorge.obiols@gmail.com>
2019-11-23 19:08:05 -0300

    [ADD] copiar atributos a los siguientes lotes

---
+++ mrp_lot_attributes/models/mrp_workorder.py
@@ -0,0 +1,49 @@
+# For copyright and license notices, see __manifest__.py file in module root
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class MrpWorkorder(models.Model):
+    _inherit = 'mrp.workorder'
+
+    def record_production(self):
+        """ Propagar los atributos al siguiente lote.
+        """
+
+        if self.qty_producing <= 0:
+            raise UserError(_('Please set the quantity you are currently '
+                              'producing. It should be different from zero.'))
+
+        if ((self.production_id.product_id.tracking != 'none') and
+                not self.final_lot_id and self.move_raw_ids):
+            raise UserError(_('You should provide a lot/serial number for '
+                              'the final product'))
+
+        for move_line in self.active_move_line_ids:
+            if (move_line.product_id.tracking != 'none'
+                    and not move_line.lot_id):
+                raise UserError(_('You should provide a lot/serial number '
+                                  'for a component'))
+
+            if self.final_lot_id.colada:
+                self.final_lot_id.colada += ', '+move_line.lot_id.colada
+            else:
+                self.final_lot_id.colada = move_line.lot_id.colada
+
+            if self.final_lot_id.tt:
+                self.final_lot_id.tt += ', '+move_line.lot_id.tt
+            else:
+                self.final_lot_id.tt = move_line.lot_id.tt
+
+            if self.final_lot_id.paquete:
+                self.final_lot_id.paquete += ', '+move_line.lot_id.paquete
+            else:
+                self.final_lot_id.paquete = move_line.lot_id.paquete
+
+            if self.final_lot_id.ot:
+                self.final_lot_id.ot += ', '+move_line.lot_id.ot
+            else:
+                self.final_lot_id.ot = move_line.lot_id.ot
+
+        super(MrpWorkorder, self).record_production()
