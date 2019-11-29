f54d62c410b348413e824230b1c18a5765ed2bc7
jobiols <jorge.obiols@gmail.com>
2019-11-29 18:00:22 -0300

    [WIP] armar wizard

--- mrp_easy_prod/wizards/prod_wizard.py
+++ mrp_easy_prod/wizards/prod_wizard.py
@@ -10,3 +10,22 @@ class WizardModel(models.TransientModel):
     name = fields.Char(
 
     )
+    state = fields.Selection(
+        [('01', 'estado 01'),
+         ('02', 'estado 02')],
+        default='01'
+    )
+
+    def next(self):
+        self.status = '02'
+
+        return {
+            'context': self.env.context,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mrp.wizard',
+            'res_id': self.id,
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+        }
