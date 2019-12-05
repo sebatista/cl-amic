2096e021bc73f227c8dc187a58f056c4c917aeb5
jobiols <jorge.obiols@gmail.com>
2019-12-04 21:46:51 -0300

    [WIP] No funciona cambiarle el usr_id al mrp.workcenter.productivity asi que le cree un campo nuevo operator_id Traducir botones

--- mrp_easy_prod/models/mrp_workorder.py
+++ mrp_easy_prod/models/mrp_workorder.py
@@ -1,7 +1,15 @@
-# -*- coding: utf-8 -*-
 # For copyright and license notices, see __manifest__.py file in module root
 
 from odoo import api, fields, models, _
+
+
+class MrpWorkcenterProductivity(models.Model):
+    _inherit = 'mrp.workcenter.productivity'
+
+    operator_id = fields.Many2one(
+        'hr.employee',
+        help='Operador'
+    )
 
 
 class MrpWorkorder(models.Model):
@@ -11,3 +19,12 @@ class MrpWorkorder(models.Model):
         'hr.employee',
         help='Operador'
     )
+
+    def button_start(self):
+        ret = super().button_start()
+
+        timeline_obj = self.env['mrp.workcenter.productivity']
+        timeline = timeline_obj.search([], limit=1, order='id desc')
+        timeline.operator_id = self.operator_id
+
+        return ret
