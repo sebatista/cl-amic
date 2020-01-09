57a1d55c5289c2dc688cc2508fd17f5ef45ba8af
jobiols <jorge.obiols@gmail.com>
2020-01-09 11:59:15 -0300

    [FIX] corregido bug en propagacion de lotes si el lote destino ya tiene un atributo no lo copiamos.

--- mrp_lot_attributes/models/mrp_workorder.py
+++ mrp_lot_attributes/models/mrp_workorder.py
@@ -11,6 +11,18 @@ class MrpWorkorder(models.Model):
         """ Propagar los atributos al siguiente lote.
         """
 
+        def propagate_attr(source, dest):
+            """ Mover el atributo de un lote a otro teniendo en cuenta que si
+                ya existe no lo tengo que copiar.
+            """
+            if not dest:
+                return source
+
+            if dest.find(source) == -1:
+                return dest + ', ' + source
+            else:
+                return dest
+
         if self.qty_producing <= 0:
             raise UserError(_('Please set the quantity you are currently '
                               'producing. It should be different from zero.'))
@@ -22,28 +34,18 @@ class MrpWorkorder(models.Model):
 
         for move_line in self.active_move_line_ids:
             if (move_line.product_id.tracking != 'none'
-                    and not move_line.lot_id):
+                and not move_line.lot_id):
                 raise UserError(_('You should provide a lot/serial number '
                                   'for a component'))
 
-            if self.final_lot_id.colada:
-                self.final_lot_id.colada += ', '+move_line.lot_id.colada
-            else:
-                self.final_lot_id.colada = move_line.lot_id.colada
-
-            if self.final_lot_id.tt:
-                self.final_lot_id.tt += ', '+move_line.lot_id.tt
-            else:
-                self.final_lot_id.tt = move_line.lot_id.tt
-
-            if self.final_lot_id.paquete:
-                self.final_lot_id.paquete += ', '+move_line.lot_id.paquete
-            else:
-                self.final_lot_id.paquete = move_line.lot_id.paquete
-
-            if self.final_lot_id.ot:
-                self.final_lot_id.ot += ', '+move_line.lot_id.ot
-            else:
-                self.final_lot_id.ot = move_line.lot_id.ot
+        # mover atributos
+        self.final_lot_id.colada = propagate_attr(
+            move_line.lot_id.colada, self.final_lot_id.colada)
+        self.final_lot_id.tt = propagate_attr(
+            move_line.lot_id.tt, self.final_lot_id.tt)
+        self.final_lot_id.paquete = propagate_attr(
+            move_line.lot_id.paquete, self.final_lot_id.paquete)
+        self.final_lot_id.ot = propagate_attr(
+            move_line.lot_id.ot, self.final_lot_id.ot)
 
         super(MrpWorkorder, self).record_production()
--- mrp_lot_attributes/readme/DESCRIPTION.rst
+++ mrp_lot_attributes/readme/DESCRIPTION.rst
@@ -1,2 +1,5 @@
 Este modulo extiende la funcionalidad de los lotes agregando atributos y
 comportamientos.
+
+Los atributos del lote son propagados de un lote entrante a un lote saliente
+en produccion al oprimir el boton record_production
