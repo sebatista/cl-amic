ac584e52cb3b28af0f42498a39921b467e08f7cf
jobiols <jorge.obiols@gmail.com>
2020-01-29 00:01:47 -0300

    [FIX] Actualizar datos de reporte para transferencias internas.

---
+++ pre_printed_stock_picking/models/stock_picking.py
@@ -0,0 +1,15 @@
+# For copyright and license notices, see __manifest__.py file in module root
+
+from openerp import models, fields
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    internal = fields.Boolean(
+        compute="_compute_internal"
+    )
+
+    def _compute_internal(self):
+        for reg in self:
+            reg.internal = reg.location_dest_id.usage == 'internal'
