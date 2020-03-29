6e0edb6b5cf250cc535326ad0666d4818b016f5a
jobiols <jorge.obiols@gmail.com>
2020-03-29 15:25:38 -0300

    [FIX] poner piezas / h en formulario de enrutamiento #95

--- amic_default/migrations/11.0.0.0.5/pre-migration.py
+++ amic_default/migrations/11.0.0.0.5/pre-migration.py
@@ -5,7 +5,11 @@ from openupgradelib import openupgrade
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
     """ Poner time_mode = 'manual'
-        El calculo de la duracion standdard de las operaciones debe ser manual
+        El calculo de la duracion standard de las operaciones debe ser manual
+        Calcular piezas hora por primera vez.
     """
     wc = env['mrp.routing.workcenter'].search([])
     wc.write({'time_mode': 'manual'})
+
+    for w in wc:
+        w.pzs_hour = 60 / w.time_cycle_manual if w.time_cycle_manual else 0
--- mrp_easy_prod/models/mrp_routing_workcenter.py
+++ mrp_easy_prod/models/mrp_routing_workcenter.py
@@ -1,5 +1,5 @@
 # For copyright and license notices, see __manifest__.py file in module root
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class MrpRoutingWorkcenter(models.Model):
@@ -8,3 +8,11 @@ class MrpRoutingWorkcenter(models.Model):
     time_cycle_manual = fields.Float(
         digits=(9, 9)
     )
+
+    pzs_hour = fields.Integer(
+        string="Piezas Hora"
+    )
+
+    @api.onchange('pzs_hour')
+    def onchange_pzs_hour(self):
+        self.time_cycle_manual = 60 / self.pzs_hour if self.pzs_hour else 0
