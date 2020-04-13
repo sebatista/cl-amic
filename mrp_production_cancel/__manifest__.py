b7cc5e5e2c3bd6931474b68101eb3d9a06b7e035
jobiols <jorge.obiols@gmail.com>
2020-04-13 15:51:56 -0300

    trabajando

---
+++ mrp_production_cancel/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Manufacturing Order Cancel/Reverse in Odoo',
+    'version': '13.0.0.0',
+    'category': 'Manufacturing',
+    'summary': 'This module helps reverse a Done Manufacturing Order, '
+               'allow to cancel Manufacturing Order and set to draft.' ,
+    'author': 'jeo Software',
+    'depends': ['base', 'mrp'],
+    'data': ["views/mrp_production_views.xml",],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    "images":['static/description/Banner.png'],
+    "live_test_url":'https://youtu.be/FSyn6axMrhA',
+}
