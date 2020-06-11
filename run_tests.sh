93ee68866c83425aa9bb41139584a80dd864a54c
jobiols <jorge.obiols@gmail.com>
2020-06-11 01:33:25 -0300

    [FIX] run_test

--- run_tests.sh
+++ run_tests.sh
@@ -14,8 +14,7 @@ sudo docker run --rm -it \
     -v /odoo_ar/odoo-11.0/extra-addons:/opt/odoo/extra-addons \
     -v /odoo_ar/odoo-11.0/dist-packages:/usr/lib/python3/dist-packages \
     -v /odoo_ar/odoo-11.0/dist-local-packages:/usr/local/lib/python3.7/dist-local-packages \
-    -e WDB_SOCKET_SERVER=wdb \
     --link pg-amic:db \
     jobiols/odoo-jeo:11.0.debug -- \
         -i  mrp_easy_prod,mrp_lot_attributes,mrp_ot,mrp_production_cancel,pre_printed_stock_picking \
-   --stop-after-init -d amic_test --test-enable
+    --stop-after-init -d amic_test --test-enable
